c2859e0390658449c3e2d986a7e11f63100b5756
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2018-12-22 18:48:51 +0100

    added verions params

--- analytics_platform/sequila/run.sh
+++ analytics_platform/sequila/run.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
-D_UID=${UID} D_GID=${GROUPS} docker-compose up -d
+#!/usr/bin/env bash -x
+D_UID=${UID} D_GID=${GROUPS} D_SEQUILA_VERSION=0.5.2 D_METASTORE_VERSION=1.2.x docker-compose up -d
