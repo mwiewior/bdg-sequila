36159ed97ef4f667abbc1be62bb0ffdcc7d1b674
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2019-11-24 16:42:11 +0100

    Removing V2 datasource

--- src/main/scala/org/biodatageeks/datasources/BAM/V2/BAMDataSourceReader.scala
+++
@@ -1,5 +0,0 @@
-package org.biodatageeks.datasources.BAM.V2
-
-class BAMDataSourceReader {
-
-}
--- src/main/scala/org/biodatageeks/datasources/BAM/V2/BAMDataSourceV2.scala
+++
@@ -1,8 +0,0 @@
-package org.biodatageeks.datasources.BAM
-
-import org.apache.spark.sql.sources.v2.reader.DataSourceReader
-import org.apache.spark.sql.sources.v2.{DataSourceOptions, DataSourceV2, ReadSupport}
-
-class BAMDataSourceV2  extends DataSourceV2  with ReadSupport {
-  override def createReader(options: DataSourceOptions): DataSourceReader = ???
-}
