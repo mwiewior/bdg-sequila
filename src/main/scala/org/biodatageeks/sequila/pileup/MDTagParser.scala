fbd416d0569cc834347fd4ecf69c3d4e4634c285
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2020-01-26 14:34:07 +0100

    Fixing deletions p1

--- src/main/scala/org/biodatageeks/sequila/pileup/MDTagParser.scala
+++ src/main/scala/org/biodatageeks/sequila/pileup/MDTagParser.scala
@@ -13,17 +13,19 @@ import org.seqdoop.hadoop_bam.BAMBDGInputFormat
 import scala.collection.mutable.ArrayBuffer
 import collection.JavaConverters._
 
+import util.control.Breaks._
+
 case class MDOperator(length: Int, base: Char) //S means to skip n positions, not fix needed
 
 object MDTagParser extends BDGAlignFileReaderWriter[BAMBDGInputFormat]{
 
   val logger: Logger = Logger.getLogger(this.getClass.getCanonicalName)
-  final val fasta = new IndexedFastaSequenceFile(new File("/Users/marek/data/Homo_sapiens_assembly18.fasta"))
+  final val fasta = new IndexedFastaSequenceFile(new File("/Users/marek/data/hs37d5.fa"))
 
 
 
   private def getReferenceFromRead(r: SAMRecord) ={
-    logger.debug(s"Processing read: ${r.getReadName}")
+    logger.info(s"Processing read: ${r.getReadName}")
     logger.debug(s"MD: ${r.getAttribute("MD")}" )
     logger.debug(s"CIGAR: ${r.getCigarString}" )
     logger.debug(s"seq: ${r.getReadString}")
@@ -34,16 +36,36 @@ object MDTagParser extends BDGAlignFileReaderWriter[BAMBDGInputFormat]{
     val alignmentBlocks = r.getAlignmentBlocks
     var ind = 0
     logger.debug(s"Read has ${alignmentBlocks.size()} aligment blocks to process")
-    for(b <- alignmentBlocks.asScala){
-      val contig = r.getContig
-      val refStart = b.getReferenceStart
-      val refEnd = b.getReferenceStart + b.getLength - 1
-      val reconstructedSeq = reconstructReference(r.getReadString, r.getCigar, r.getAttribute("MD").toString, ind)
-      val isEqual = compareToRefSeq(reconstructedSeq, contig, refStart, refEnd)
-      logger.debug(s"Comparing to ref ${isEqual.toString.toUpperCase}")
-      if (!isEqual) throw new Exception("Bad ref seq reconstruction")
-      ind += 1
+    if (alignmentBlocks !=  null  && alignmentBlocks.size() > 0){
+      for(b <- alignmentBlocks.asScala){
+        breakable {
+          val deletions = {
+            var alignBlockCnt = 0
+            var delCnt = 0
+            val cigar = r.getCigar.iterator()
+            while(cigar.hasNext && alignBlockCnt <= ind){
+              val el = cigar.next()
+              if(el.getOperator == CigarOperator.MATCH_OR_MISMATCH)  alignBlockCnt += 1
+              else if(el.getOperator == CigarOperator.DELETION) delCnt += el.getLength
+            }
+            delCnt
+          }
+          val contig = r.getContig
+          val refStart = b.getReferenceStart - deletions
+          val refEnd = b.getReferenceStart + b.getLength - 1
+          if(r.getAttribute("MD") == null) {
+            logger.error(s"MD tag for read ${r.getReadName} is missing. Skipping it.")
+            break()
+          }
+          val reconstructedSeq = reconstructReference(r.getReadString, r.getCigar, r.getAttribute("MD").toString, ind)
+          val isEqual = compareToRefSeq(reconstructedSeq, contig, refStart, refEnd)
+          logger.debug(s"Comparing to ref ${isEqual.toString.toUpperCase}")
+          if (!isEqual) throw new Exception("Bad ref seq reconstruction")
+          ind += 1
+        }
+      }
     }
+
 
 
   }
@@ -110,10 +132,18 @@ object MDTagParser extends BDGAlignFileReaderWriter[BAMBDGInputFormat]{
     while (matches.hasNext) {
       val m = matches.next().toUpperCase
       logger.debug(s"MD operator: ${m}")
-      if(m.last.isLetter){
+      if(m.last.isLetter && !m.contains('^') ){
         val skipPos = m.dropRight(1).toInt
         ab.append(MDOperator(skipPos, 'S') )
-        ab.append(MDOperator(0, m.last))
+        ab.append(MDOperator(0, m.last.toUpper))
+      }
+      else if (m.last.isLetter && m.contains('^') ){ //encoding deletions as lowercase
+        val arr =  m.split('^')
+        val skipPos = arr.head.toInt
+        ab.append(MDOperator(skipPos, 'S') )
+        arr(1).foreach { b =>
+          ab.append(MDOperator(0, b.toLower))
+        }
       }
       else ab.append(MDOperator(m.toInt, 'S') )
 
@@ -131,7 +161,7 @@ object MDTagParser extends BDGAlignFileReaderWriter[BAMBDGInputFormat]{
       logger.debug(s"Applying MD op: ${op.toString}, ${ind}")
       if(ind < blockLength + pShift) {
         if(op.base == 'S' ) {
-          logger.debug(s"Index: ${ind}, inserts:  ${inserts}, blockLen:  ${blockLength}")
+          logger.info(s"Index: ${ind}, inserts:  ${inserts}, blockLen:  ${blockLength}")
           val startPos =   { if(isFirstOpInBlock) pShift else 0 } + ind
           val shift  = {if (op.length - pShift + inserts >  blockLength) blockLength else  op.length - { if(isFirstOpInBlock) pShift - inserts else 0  }  }
           val endPos =  startPos + shift
@@ -142,9 +172,9 @@ object MDTagParser extends BDGAlignFileReaderWriter[BAMBDGInputFormat]{
           isFirstOpInBlock = false
         }
         else if(op.base != 'S' && ind >= pShift){
-          seqFixed.append(op.base.toString)
+          seqFixed.append(op.base.toUpper.toString)
           logger.debug(s"Append seq length: 1, at pos ${ind} with base ${op.base.toString}")
-          ind += 1
+          if (op.base.isUpper) ind += 1
         }
       }
       else ind += op.length
@@ -164,11 +194,13 @@ object MDTagParser extends BDGAlignFileReaderWriter[BAMBDGInputFormat]{
       .builder()
       .master("local[1]")
       .getOrCreate()
+    sparkSession.sparkContext.setLogLevel("INFO")
     val sqlSession = sparkSession.sqlContext
-    val bamRecords = readBAMFile(sqlSession,"/Users/marek/git/forks/bdg-sequila/src/test/resources/NA12878.slice.md.bam")
+    val bamRecords = readBAMFile(sqlSession,"/Users/marek/data/NA12878.chrom20.ILLUMINA.bwa.CEU.low_coverage.20121211.bam")
 
     sparkSession.time{
       val records = bamRecords
+    //      .filter(_.getReadName=="SRR622461.74266492")
         .map(getReferenceFromRead(_))
         .count()
     }
